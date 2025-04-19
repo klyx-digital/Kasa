5fa4681e1784c6856df694ccbe853deb39369472
Mouctar <Mr.conte@icloud.com>
2025-04-19 08:52:04 +0200

    modification route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -7,7 +7,7 @@ import Error404 from "../pages/Error";
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/Kasa" element={<Home />} />
       <Route path="/logement/:id" element={<Logement />} />
       <Route path="/about" element={<About />} />
       <Route path="*" element={<Error404 />} />
